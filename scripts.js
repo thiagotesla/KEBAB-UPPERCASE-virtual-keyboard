70249d5d39be19fcc084ea791b9f6679d09fe6bd
Thiago Tesla <thiagobarros95@gmail.com>
2022-04-07 17:34:03 -0300

    create KEBAB-UPPERCASE-VIRTUAL-KEYBOARD

---
+++ scripts.js
@@ -0,0 +1,90 @@
+const resultado = document.getElementById("result")
+
+function clearAll(){
+    resultado.innerText = ""
+};
+function addA(){
+    resultado.innerText += "A"
+};
+function addB(){
+    resultado.innerText += "B"
+};
+function addC(){
+    resultado.innerText += "C"
+};
+function addD(){
+    resultado.innerText += "D"
+};
+function addE(){
+    resultado.innerText += "E"
+};
+function addF(){
+    resultado.innerText += "F"
+};
+function addG(){ 
+    resultado.innerText += "G"
+};
+function addH(){ 
+    resultado.innerText += "H"
+};
+function addI(){ 
+    resultado.innerText += "I"
+};
+function addJ(){ 
+    resultado.innerText += "J"
+};
+function addK(){ 
+    resultado.innerText += "K"
+};
+function addL(){ 
+    resultado.innerText += "L"
+};
+function addM(){ 
+    resultado.innerText += "M"
+};
+function addN(){ 
+    resultado.innerText += "N"
+};
+function addO(){ 
+    resultado.innerText += "O"
+};
+function addP(){ 
+    resultado.innerText += "P"
+};
+function addQ(){ 
+    resultado.innerText += "Q"
+};
+function addR(){ 
+    resultado.innerText += "R"
+};
+function addS(){ 
+    resultado.innerText += "S"
+};
+function addT(){ 
+    resultado.innerText += "T"
+};
+function addU(){ 
+    resultado.innerText += "U"
+};
+function addV(){ 
+    resultado.innerText += "V"
+};
+function addW(){ 
+    resultado.innerText += "W"
+};
+function addX(){ 
+    resultado.innerText += "X"
+};
+function addY(){ 
+    resultado.innerText += "Y"
+};
+function addZ(){ 
+    resultado.innerText += "Z"
+};
+function addSpace(){ 
+    resultado.innerText += "-"
+};
+
+function backSpace(){
+    resultado.innerText = resultado.innerText.slice(0, -1)
+}
